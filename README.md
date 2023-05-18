464d644ba33ab579be149b3d14a26a43ea2b456c
2-3-5-7 <>
2023-05-18 18:04:00 +0800

    ver 1.0.0

---
+++ README.md
@@ -0,0 +1,14 @@
+# hexo-mermaid-lastest
+
+- All Hexo Mermaid packages are outdated. I referred to the latest [Mermaid documentation](https://mermaid.js.org/intro/n00b-gettingStarted.html#_3-calling-the-javascript-api) and made some modifications to the [hexo-filter-mermaid-diagrams-v](https://www.npmjs.com/package/hexo-filter-mermaid-diagrams-v) plugin. The code is simple and easy to understand.
+
+- Install `npm i hexo-mermaid-lastest`
+
+- Use mermaid codeblocks in markdown files
+
+	````
+	```mermaid
+	flowchart LR
+		A --> B
+	```
+	````
---
+++ index.js
@@ -0,0 +1,30 @@
+hexo.extend.filter.register('before_post_render', render, 9);
+
+function ignore(data) {
+	let source = data.source;
+	let ext = source.substring(source.lastIndexOf('.')).toLowerCase();
+	return ['.js', '.css', '.html', '.htm'].indexOf(ext) > -1;
+}
+
+function render(data) {
+	let reg = /(\s*)(```) *(mermaid) *\n?([\s\S]+?)\s*(\2)(\n+|$)/g;
+
+	if (!ignore(data)) {
+		let matched = false;
+		data.content = data.content
+			.replace(reg, function(raw, start, startQuote, lang, content, endQuote, end) {
+				matched = true;
+				return start + '<pre class="mermaid">' + content + '</pre>' + end;
+				// use this if you also need to display the source code
+				// return start + '<pre class="mermaid">'+content+'</pre>' + end + raw;
+			});
+		if (matched) {
+			// https://mermaid.js.org/intro/n00b-gettingStarted.html#_3-calling-the-javascript-api
+			// always use latest one
+			data.content += `<script type="module">
+								import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.esm.min.mjs';
+								mermaid.initialize({ startOnLoad: true });
+							  </script>`;
+		}
+	}
+};
